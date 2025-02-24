459e5215e35b3bfba0d02336acefc119267b3ca9
artagaz <and.vaylin@gmail.com>
2025-02-24 08:34:38 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#1
+# my_list = input().split()
+# list1 = [i for i in my_list if int(i) < 5]
+# list2 = [int(i)/2 for i in my_list]
+# list3 = [int(i)*2 for i in my_list if int(i) > 17]
+# list4 = [i*i for i in range(0, int(input()))]
+#print(*[int(i)*int(i) for i in input().split()])
+#print(*[int(i)*int(i) for i in input().split() if int(i)%2 != 0 and str(int(i)*int(i))[-1] != '9'])
+
+#2
