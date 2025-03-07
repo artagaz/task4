ebce676f985d34b0f39734f467effad73ca76282
artagaz <and.vaylin@gmail.com>
2025-03-07 19:45:35 +0700

    10

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 # list4 = [i*i for i in range(0, int(input()))]
 #print(*[int(i)*int(i) for i in input().split()])
 #print(*[int(i)*int(i) for i in input().split() if int(i)%2 != 0 and str(int(i)*int(i))[-1] != '9'])
+from dataclasses import field
 from idlelib.replace import replace
 
 
@@ -55,3 +56,72 @@ from idlelib.replace import replace
 #
 # print(palindrome('а роза упала на лапу Азора'))
 # print(palindrome('не палиндром'))
+
+#8
+# def tik_tac_toe(game_field):
+#     values = []
+#     for i in game_field[0] + game_field[1] + game_field[2]:
+#         values.append(i)
+#     # horizontal
+#     for i in game_field:
+#         if i[0] == i[1] == i[2]:
+#             return f'{i[0]} win'
+#     # vertical
+#     for i in range(0, 3):
+#         print(values[0 + i:7 + i:3])
+#         if values[0+i:7+i:3] == ['0', '0', '0']:
+#             return "0 win"
+#         if values[0+i:7+i:3] == ['x', 'x', 'x']:
+#             return "x win"
+#     # diagonal
+#     if (values[0] == values[4] == values[8]) or (values[2] == values[4] == values[6]):
+#         return f'{values[4]} win'
+#     return 'draw'
+#
+# data = '''0 - 0
+# x x x
+# 0 0 -'''
+# field = [line.split() for line in data.split('\n')]
+# print(tik_tac_toe(field))
+
+#9
+# memory = []
+# def print_without_duplicates(message):
+#   if message not in memory:
+#     print(message)
+#     memory.append(message)
+#
+# print_without_duplicates("Привет")
+# print_without_duplicates("Не могу до тебя дозвониться")
+# print_without_duplicates("Не могу до тебя дозвониться")
+# print_without_duplicates("Не могу до тебя дозвониться")
+# print_without_duplicates("Когда доедешь до дома")
+# print_without_duplicates("Ага, жду")
+# print_without_duplicates("Ага, жду")
+
+#10
+persons = {}
+
+def add_friends(name_of_person, list_of_friends):
+    a = persons.get(name_of_person)
+    if a:
+        persons[name_of_person] = a + list_of_friends
+    else:
+        persons[name_of_person] = list_of_friends
+
+def are_friends(name_of_person1, name_of_person2):
+    if name_of_person2 in persons[name_of_person1]:
+        return True
+    return False
+
+def print_friends(name_of_person):
+    s = reversed(persons[name_of_person])
+    for i in s:
+        print(i, end=' ')
+    print()
+
+add_friends("Алла", ["Марина", "Иван"])
+print(are_friends("Алла", "Мария"))
+add_friends("Алла", ["Мария"])
+print(are_friends("Алла", "Мария"))
+#11
